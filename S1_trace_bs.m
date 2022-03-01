dfa3e6eab5283b2adcf43f19a3bb75f35fd9c36b
Hao Zhang <zhsess@gmail.com>
2022-02-28 18:14:58 -0800

    2-28

--- S1_trace_bs.m
+++ S1_trace_bs.m
@@ -8,14 +8,15 @@ end
 % event15
 t_0 = 390; dt = 30;
 
-dist_bs = zeros(20,13);
-e1 = [];
+dist_bs = zeros(100,13);
+th_mcfs = zeros(100,13);
+th_bfs = zeros(100,13);
 e2 = [];
 t = 390:30:750;
 
-for i = 1:20 % times of boot-strap
-    randI_BF = rand(5151,1)<0.7;
-    randI_MCF = rand(4851,1)<0.7;
+for i = 1:100 % times of boot-strap
+    randI_BF = rand(5151,1)<0.6;
+    randI_MCF = rand(4851,1)<0.6;
     disp(i)
     for fig_idx = 1:13
         load([event_dir 'mw/' num2str(fig_idx) '_BF.mat'])
@@ -52,10 +53,16 @@ for i = 1:20 % times of boot-strap
         dist_2 = -(ey2*x2-ex2*y2)/(ex*ey2-ey*ex2);
         dist = (dist_1+dist_2)/2;
         dist_bs(i, fig_idx) = dist;
+
+        th_bf = 90 - atan(ey1/ex1)*180/pi;
+        th_mcf = 90 - atan(ey2/ex2)*180/pi;
+        th_bfs(i, fig_idx) = th_bf;
+        th_mcfs(i, fig_idx) = th_mcf;
+
     end
 end
 
-save([event_dir 'mw/dists_bs.mat'], 'dists', 't')
+save([event_dir 'mw/dists_bs.mat'], 'dist_bs', 'th_mcfs', 'th_bfs')
 
 
 
